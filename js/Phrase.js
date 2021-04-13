2a01292d1c5c659ec5ba79b56b4c5c451efbd2fe
IsaacCurry03 <73970390+IsaacCurry03@users.noreply.github.com>
2021-04-13 12:15:33 -0700

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,49 +1,43 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
-class Phrase {
-  constructor(phrase) {
-    this.phrase = phrase.toLowerCase();
-  }
-
-  addPhraseToDisplay() {
-    const phraseDiv = document.querySelector('#phrase ul');
-
-    // loop through the phrase and add for every letter or space a list element to the unorderd list
-    for (let i = 0; i < this.phrase.length; i += 1) {
-      let li = document.createElement('li');
-      let char = this.phrase[i];
-
-      if (char === ' ') {
-        // an li with a space gets the 'space' class
-        li.className = 'hide space';
-      } else {
-        // an li with a letter gets the 'letter' class
-        li.className = `hide letter ${char}`;
-      }
-      li.textContent = char;
-      phraseDiv.appendChild(li);
+ class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
     }
-  }
-
-  checkLetter(letter) {
-    // check if the guessed letter is in the phrase
-    if (this.phrase.includes(letter)) {
-      return true;
-    } else {
-      return false;
+    addPhraseToDisplay() {
+        //create phraseContainer element and create element for the first element child, (ul)
+        const phraseContainer = document.getElementById("phrase");
+        const ul = phraseContainer.firstElementChild;
+        // console.log(ul)
+        for(let i = 0; i < this.phrase.length; i += 1) {
+            //create list element to store phrase
+            const letter = document.createElement("LI");
+            //employ innerHTML to display letters and spaces in this.phrase
+            letter.innerHTML = this.phrase[i];
+            //add letter class if character is not space
+            if (letter.innerHTML !== ' ') {
+                letter.classList.add("letter");
+            //add space class if character is not a letter
+            } else  {
+                letter.classList.add("space");
+            }
+            //append the letter element to the ul
+            ul.appendChild(letter);
+        }
     }
-  }
-
-  showMatchedLetter(letter) {
-    const letters = document.querySelectorAll('#phrase li');
-    // loop through the phrase and give any matched letter the 'show' class
-    letters.forEach(key => {
-      if (key.textContent === letter) {
-        key.setAttribute('class', 'show');
-      }
-    });
-  }
-}
-
-
+    //Check to see if the letter is included in the active phrase
+    checkLetter(letter) {    
+        if (this.phrase.includes(letter)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    showMatchedLetter(letter) {    
+        //find all letter elements
+        const letters = document.querySelectorAll(".letter");
+        for (let i = 0; i < letters.length; i ++) {//loop over letters and test against letter parameter
+            if (letters[i].innerHTML.toLowerCase() === letter.toLowerCase()) {
+                letters[i].className = "show";//changes class to show
+            }
+        }
+     } 
+ }  
